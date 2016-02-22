f19cdfb55dd66030cae433c66c32823b1bf9f899
Josh <jkuey24>
2016-02-22 15:52:36 -0800

    Country SQL

---
+++ sql_practice.sql
@@ -0,0 +1,21 @@
+﻿INSERT INTO
+ persons(
+ given_name,
+ family_name,
+ dob,
+ address,
+ email)
+VALUES(
+ 'Ruby',
+ 'Rhod',
+ '2017-06-04',
+ '72 Capital St Cityvielle Earth',
+ 'ruby_rhod@gmail.com'
+ ),
+ ( 
+ 'Emanuelle',
+ 'Zorg',
+ '2001-03-02',
+ '1234 Big Drive San Diego Earth',
+ 'bigd@gmail.com'
+ );
